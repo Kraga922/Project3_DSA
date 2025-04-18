237f7f42095ae1c4853955059d1d66637a77b004
rohph <roh.pherwani@gmail.com>
2025-04-18 17:52:23 -0400

    Update dijkstras to return path

--- dijkstras.py
+++ dijkstras.py
@@ -2,7 +2,7 @@ import math
 import queue
 import networkx as nx
 
-def dijkstrasAlgorithm(G, start):
+def dijkstrasAlgorithm(G, start, destination):
     #Set up list of nodes, visited set, and priority queue
     result = {}
     visited = {start}
@@ -11,7 +11,7 @@ def dijkstrasAlgorithm(G, start):
 
     #Add source to priority queue and create an entry in result for each vertex
     pq.put(0, start)
-    for (vertex in G.nodes):
+    for vertex in G.nodes:
         if vertex not in visited:
             result[vertex] = [math.inf, -1]
             pq.put(math.inf, vertex)
@@ -21,14 +21,29 @@ def dijkstrasAlgorithm(G, start):
         current = pq.get()
         visited.add(current)
         distance = result[current][0]
-        for (v in G.neighbors(current)):
+        for v in G.neighbors(current):
             if v not in visited:
                 pq.put(v)
                 if (distance + G[current][v]["distance"] < result[v][0]):
                     result[v][0] = distance + G[current][v]["distance"]
                     result[v][1] = current
 
-    return result
+    path = []
+
+    #Get path from Dijkstra's output as a list of each node in the path
+    currNode = destination
+    while currNode != start:
+        path.append(currNode)
+
+        if (result[currNode][1] == -1):
+            raise ValueError(f"No path exists")
+
+        currNode = result[currNode][1]
+
+    path.append(start)
+    path.reverse()
+
+    return path
 
 
 
